e7dd22d5db5cf42c352ae994019682d370607f99
Travis Glines <travisglines@gmail.com>
2014-09-18 13:46:12 -0700

    Swap uses of sed for grep to not import certain files

--- scssImport.sh
+++ scssImport.sh
@@ -3,8 +3,8 @@
 # find web/css -type f | \                            List all files under web/css
 # awk '{ printf "@import \x27%s\x27;\n", $1 }' | \    Wrap the filepath string with @import('')
 # sed "s/web\/css\///g" | \                           Remove the web/css part of the file path
-# sed "/application/d" | \                            Don't include application.scss (prevent including itself)
-# sed "/\.css/d" | \                                  Don't include .css files and .css.map file
+# grep -v application.scss | \                        Don't include application.scss (prevent including itself)
+# grep -v "\.css" | \                                 Don't include .css files and .css.map file
 # sed "s/.scss//g" > \                                Remove the .scss file extension from includes
 # web/css/application.scss                            Write out to application.scss and overwrite what's there
 
@@ -14,8 +14,8 @@ SCSS_FILES=$( \
 find web/css -type f | \
 awk '{ printf "@import \x27%s\x27;\n", $1 }' | \
 sed "s/web\/css\///g" | \
-sed "/application/d" | \
-sed "/\.css/d" | \
+grep -v application.scss | \
+grep -v "\.css" | \
 sed "s/.scss//g" \
 )
 
