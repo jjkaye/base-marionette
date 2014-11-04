33d03150d51950f7c20d229fe382102db26c900e
Bret Morris <bret@useallfive.com>
2014-11-04 12:00:36 -0800

    Style sample contacts app.

--- web/js/apps/contacts/components/list/views/collection.js
+++ web/js/apps/contacts/components/list/views/collection.js
@@ -5,6 +5,7 @@ define([
     './empty'
 ], function(App, Marionette, ItemView, EmptyView) {
     return Marionette.CollectionView.extend({
+        className: 'contacts-list',
         tagName: 'ul',
         childView: ItemView,
         emptyView: EmptyView
